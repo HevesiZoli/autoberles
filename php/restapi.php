bab3d39d1545ebc88e84745eb48aeb5dad7dbed5
Tocsa18 <elevetoth@gmail.com>
2026-02-13 22:14:08 +0100

    Update restapi.php

--- php/restapi.php
+++ php/restapi.php
@@ -6,4 +6,11 @@ include('naplo.php');
 include('adatbaziskapcsolat.php'); 
 include('jarmuvek.php'); // $autok objektum létrehozása
 
+$uzenet= file_get_contents('php://input');
+
+if(isset($uzenet) && !empty($uzenet))
+{
+	 $naplo->_bejegyez('Rest kérés érkezett:  '.$uzenet);
+}
+
 ?>
